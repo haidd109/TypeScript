563bcaff6c02f76e7c5ffe0d4014ff9e87403951
Đinh Đức Hải <77098196+haihamhb1@users.noreply.github.com>
2022-04-10 19:26:43 +0700

    use api

--- api/index.js
+++ api/index.js
@@ -1,13 +1,20 @@
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose")
+const dotenv = require("dotenv");
+const userRoute = require("./routes/user")
 
-mongoose.connect("mongodb+srv://haidd109:Haidz2002@cluster0.n5fnn.mongodb.net/shop?retryWrites=true&w=majority"
-).then(() => console.log("Ket noi DB thanh cong!"))
- .catch((err) => {
-     console.log("err");
- })
+dotenv.config();
 
-app.listen(5000,  ()=> {
+mongoose
+    .connect(process.env.MONGO_URL)
+    .then(() => console.log("Ket noi DB thanh cong!"))
+    .catch((err) => {
+        console.log("err");
+    });
+
+app.use("/api/user", userRoute);
+
+app.listen(process.env.PORT || 5000,  ()=> {
     console.log("Backend server is running!");
 })
