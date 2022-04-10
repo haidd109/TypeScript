1c7b7684d62c889884d08d5d9adb31d6bcf89467
Đinh Đức Hải <77098196+haihamhb1@users.noreply.github.com>
2022-04-10 22:38:04 +0700

    Login

--- api/routes/auth.js
+++ api/routes/auth.js
@@ -1,8 +1,9 @@
 const router = require("express").Router();
 const User = require("../model/User")
 const CryptoJS = require("crypto-js");
+const jwt = require("jsonwebtoken");
 
-//DangKi
+//Register
 router.post("/register", async (req,res)=>{
     const newUser = new User({
         username: req.body.username,
@@ -21,5 +22,35 @@ router.post("/register", async (req,res)=>{
     }
 
 });
+
+//Login
+
+router.post("/login", async (req,res) => {
+    try{
+        const user = await User.findOne({ username: req.body.username });
+        !user && res.status(401).json("User khonh chinh xac")
+
+        const hashedPassword = CryptoJS.AES.decrypt(
+            user.password,
+            process.env.PASS_SEC
+            );
+        const Originalpassword = hashedPassword.toString(CryptoJS.enc.Utf8);
+
+        Originalpassword !== req.body.password &&
+         res.status(401).json("Password khong chinh xac")
+        
+        const accessToken = jwt.sign({
+            id: user._id,
+            isAdmin: user.isAdmin,
+        }, process.env.JWT_SEC,
+            {expiresIn:"1d"})
+
+        const { password, ...other } = user._doc;
+
+        res.status(200).json({...other, accessToken});
+    } catch(err) {
+        res.status(500).json(err);
+    }
+})
 
 module.exports = router;
